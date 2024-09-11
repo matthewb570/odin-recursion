586421a192dfcd56ce96699cb4d93c7eebdbd065
Matthew Burgess <boog397@gmail.com>
2024-09-10 23:15:00 -0500

    Add mergeSort recursive function

--- src/js/index.js
+++ src/js/index.js
@@ -33,6 +33,41 @@ function fibsRec(num) {
     return fibArray;
 }
 
+function mergeSort(array) {
+    if (array.length === 1) {
+        return array;
+    }
+
+    let arrayMidPoint = Math.round(array.length / 2);
+
+    let leftArraySorted = mergeSort(array.slice(0, arrayMidPoint));
+    let rightArraySorted = mergeSort(array.slice(arrayMidPoint, array.length));
+
+    let arraySorted = [];
+    let leftArrayIndex = 0;
+    let rightArrayIndex = 0;
+    while (leftArrayIndex < leftArraySorted.length || rightArrayIndex < rightArraySorted.length) {
+        let leftCandidate;
+        let rightCandidate;
+        if (leftArrayIndex < leftArraySorted.length) {
+            leftCandidate = leftArraySorted[leftArrayIndex];
+        }
+        if (rightArrayIndex < rightArraySorted.length) {
+            rightCandidate = rightArraySorted[rightArrayIndex];
+        }
+
+        if (rightCandidate == undefined || (leftCandidate != undefined && leftCandidate <= rightCandidate)) {
+            arraySorted.push(leftCandidate);
+            leftArrayIndex++;
+        } else {
+            arraySorted.push(rightCandidate);
+            rightArrayIndex++;
+        }
+    }
+    
+    return arraySorted;
+}
+
 console.log(fibs(0));
 console.log(fibs(1));
 console.log(fibs(2));
@@ -42,3 +77,6 @@ console.log(fibsRec(0));
 console.log(fibsRec(1));
 console.log(fibsRec(2));
 console.log(fibsRec(8));
+
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
+console.log(mergeSort([105, 79, 100, 110]));
